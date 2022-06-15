a3a331e159fde9d70a5cb73998f686639dbccbc9
ShingiraiBhengesa <72065045+ShingiraiBhengesa@users.noreply.github.com>
2022-06-15 04:15:06 +0300

    render rockets to the UI

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import RocketItem from './RocketsItem';
 
-function Rockets() {
- return <div>To the moon</div>;
-  }
-
-  export default Rockets;
+export default function Rockets() {
+  const rocketData = useSelector((state) => state.rocketsReducer);
+  return (
+    <div className="rocket-container flex">
+      {rocketData.map((rocket) => (
+        <RocketItem
+          id={rocket.id}
+          key={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          image={rocket.image}
+          reserved={rocket.reserved}
+        />
+      ))}
+    </div>
+  );
+}
